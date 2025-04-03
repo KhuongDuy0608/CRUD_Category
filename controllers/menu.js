c25229dc1013b5f66970857c23c6fc286ae4f94c
khuongduy <khuongduy0608@gmail.com>
2025-04-03 10:23:31 +0700

    Them menu

--- controllers/menu.js
+++ controllers/menu.js
@@ -4,10 +4,8 @@ module.exports = {
 
     GetAllMenu: async function () {
         try {
-            // Lấy tất cả các menu chưa bị xóa
             let menuList = await menuModel.find({ isDeleted: false });
 
-            // Chuyển đổi dữ liệu thành một object map với _id là key
             let menuMap = {};
             menuList.forEach(menu => {
                 menuMap[menu._id] = {
@@ -17,21 +15,18 @@ module.exports = {
                 };
             });
 
-            // Tạo cây menu bằng cách gán các mục con vào `children` của mục cha
             let rootMenus = [];
             menuList.forEach(menu => {
                 if (menu.parent) {
-                    // Nếu menu có parent, thêm vào children của parent
                     if (menuMap[menu.parent]) {
                         menuMap[menu.parent].children.push(menuMap[menu._id]);
                     }
                 } else {
-                    // Nếu không có parent => đây là menu cấp cao nhất
                     rootMenus.push(menuMap[menu._id]);
                 }
             });
 
-            return rootMenus;  // Trả về cây menu đã xây dựng
+            return rootMenus; 
         } catch (error) {
             throw new Error(error.message);
         }
